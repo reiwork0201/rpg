89c37318eed636f088db46a6f12b74dd3791038b
reiwork0201 <reiki0201.work@gmail.com>
2025-08-01 14:01:55 +0900

    Update workbox config to support offline runtime caching

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  globDirectory: './',   
+  globDirectory: './',
   globPatterns: [
     '**/*.{html,js,json,wasm,png,svg,css,ogg,mp3,woff2,xyz,mid,wav,exe,lmu,ini,ldb,lmt,txt}',
-    'games/**/*.*'             
+    'games/**/*.*'
   ],
-  swDest: 'public/sw.js',     
+  swDest: 'public/sw.js',
   clientsClaim: true,
   skipWaiting: true,
-  maximumFileSizeToCacheInBytes: 1 * 1024 * 1024 * 1024
+  maximumFileSizeToCacheInBytes: 1024 * 1024 * 1024,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:wasm|exe|json|ldb|lmu|lmt|ini|mid|ogg|mp3|png|svg|js|css|txt)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'game-assets-cache',
+        expiration: {
+          maxEntries: 3000,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30日
+        }
+      }
+    }
+  ]
 };
